fed27354757d810dcf9ab6482b0895d3d5e36344
skullzado <pafcalzado@gmail.com>
2025-09-09 17:44:48 +0800

    Add commands for repl func

--- repl.go
+++ repl.go
@@ -20,8 +20,18 @@ func startRepl() {
 		}
 
 		commandName := words[0]
+		command, ok := getCommands()[commandName]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -34,4 +44,25 @@ func cleanInput(text string) []string {
 		words[index] = w
 	}
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
